81cb00d996069ad68005113d386cab375114572d
ChaseTerzian <chaseterzian@gmail.com>
2015-03-25 11:46:27 -0600

    tests pass, prelim index setup

--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -1,8 +1,8 @@
 def create_user(overrides ={})
   User.create!({
-    :first_name => 'Rick'
-    :last_name => 'Grimes'
-    :email => 'walkingdead@email.com'
+    :first_name => 'Rick',
+    :last_name => 'Grimes',
+    :email => 'walkingdead@email.com',
     :password => 'Karl!'
   }.merge(overrides))
 end
@@ -10,7 +10,7 @@ end
 def create_post(user, overrides ={})
   Post.create!({
     :title => 'Kill Zombies',
-    :description => 'Watch out for bad people and kill all zombies'
+    :description => 'Watch out for bad people and kill all zombies',
     :user_id => user.id
   }.merge(overrides))
 end
