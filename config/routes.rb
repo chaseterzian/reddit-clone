2e83992adb79256cce2986f875d9795c6f1145ab
ChaseTerzian <chaseterzian@gmail.com>
2015-03-25 13:40:53 -0600

    started auth, added reg,

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
     root 'posts#index'
 
+    get '/sign-up' => 'registrations#new', as: :signup
+    post '/sign-up' => 'registrations#create'
+    get '/sign-in' => 'authentication#new', as: :signin
+    post '/sign-in' => 'authentication#create'
+    get '/sign-out' => 'authentication#destroy', as: :signout
+
     resources :posts
 
 end
